97efc0edc3c41efd7a5abf67a1c8bb3ae731e5ef
guusgrimbergen <38925598+guusgrimbergen@users.noreply.github.com>
2018-06-05 11:30:01 +0200

    Delete testcorr.m

--- testcorr.m
+++
@@ -1,6 +0,0 @@
-pvalues=array2table(zeros(1, 2286));
-pvalues.Properties.VariableNames=finalmatrix.Properties.VariableNames(5:end);
-for i=5:2290
-    [~, p]=corrcoef(finalmatrix{:,4}, finalmatrix{:,i});
-    pvalues(1,i-4)=array2table(p(1,2));
-end
